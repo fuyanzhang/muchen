f43f35143a4f9097e12457492fecc37e49d1d5c8
yanzhang.fu@yoho.cn <yanzhang.fu@yoho.cn>
2018-07-06 10:51:44 +0800

    服务端代码

---
+++ server/src/test/java/com/muchen/server/test/TestAnnotation.java
@@ -0,0 +1,16 @@
+package com.muchen.server.test;
+
+import com.muchen.server.annotation.ExportService;
+
+/**
+ * Author:yanzhang.fu
+ * Date:2018/7/5
+ * Description:
+ * Modified By：
+ **/
+@ExportService(name="testanno")
+public class TestAnnotation {
+    public void test(){
+
+    }
+}
